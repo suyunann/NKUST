45521ff6eaf869ec4e396591f3b1c245135280de
suyuu <93572182+suyunann@users.noreply.github.com>
2021-12-14 17:38:09 +0800

    Delete NKUST_C110181133 directory

--- NKUST_C110181133/java_class_tutorial/Tutorial_class/src/Class_ex2.java
+++
@@ -1,9 +0,0 @@
-public class Class_ex2 {
-    public static void main(String[] args) {
-        Car myCar = new Car();
-        myCar.setCar("gray","Benz");
-        myCar.setCar(10000);
-        myCar.setCar(2999.99);
-        myCar.showMyCar();
-    }
-}
