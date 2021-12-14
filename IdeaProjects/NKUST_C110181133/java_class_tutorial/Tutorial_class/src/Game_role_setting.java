d6fe24d53c719378ecb884414988793387fc218e
suyuu <93572182+suyunann@users.noreply.github.com>
2021-12-14 17:00:03 +0800

    Add files via upload

--- IdeaProjects/NKUST_C110181133/java_class_tutorial/Tutorial_class/src/Game_role_setting.java
+++ IdeaProjects/NKUST_C110181133/java_class_tutorial/Tutorial_class/src/Game_role_setting.java
@@ -1,46 +1,11 @@
-public class Game_role_setting {
-    String name ;
-    int hp;
-    short atk;
-    byte mp;
-    String skill_atk;
-    String sing;
-    String weapon;
-    void setSing(String sing){
-        this.sing = sing;
-    }
-    void setWeapon(String weapon){
-        this.weapon=weapon;
-    }
-    String getName()  {
-        return this.name;
-    }
+public class Game_role_setting extends RoleSetting{
 
-    void ShoutMyName() {
 
-        System.out.println("我的職業是:" + getName().toUpperCase() + "!!!!!!!!!!!" );
-        System.out.println("HP:" + hp + "Atk:" + atk + "MP:" + mp);
-        System.out.println("我的攻擊是:"+this.atk);
-        System.out.println("詠唱:"+sing);
-        System.out.println(("穿戴:" + weapon));
+    Game_role_setting(String Name, int Hp, int Atk, int Mp, String Weapon, String Sing, String Skill_atk) {
+        super(Name,Hp,Atk,Mp,Weapon,Sing,Skill_atk);
     }
 
 
-    void setRole(int hp){
-        this.hp = hp;
-    }
-
-    void setRole(short atk){
-        this.atk = atk;
-    }
-
-    void setRole(byte mp){
-        this.mp = mp;
-    }
-
-    void setAtk(String skill_atk){
-        this.skill_atk=skill_atk;
-    }
 
 
 }
--- IdeaProjects/NKUST_C110181133/java_class_tutorial/Tutorial_class/src/game.java
+++ IdeaProjects/NKUST_C110181133/java_class_tutorial/Tutorial_class/src/game.java
@@ -1,32 +1,8 @@
 public class game {
     public static void main(String[] args) {
-        Game_role_setting Archer = new Game_role_setting();
-        Game_role_setting Berserker = new Game_role_setting();
-        Game_role_setting Mage = new Game_role_setting();
-
-        Archer.name = "弓箭手";
-        Archer.hp = 100;
-        Archer.atk = 50;
-        Archer.mp = 20;
-        Archer.weapon = "腐蝕箭";
-        Archer.sing = "集中";
-        Archer.skill_atk = "連續射擊";
-
-        Berserker.name = "狂戰士";
-        Berserker.hp = 150;
-        Berserker.atk = 60;
-        Berserker.mp = 10;
-        Berserker.weapon = "戰斧";
-        Berserker.sing = "衝鋒";
-        Berserker.skill_atk = "旋風斬";
-
-        Mage.name = "魔法師";
-        Mage.hp = 60;
-        Mage.atk = 70;
-        Mage.mp = 100;
-        Mage.weapon = "法杖";
-        Mage.sing = "火焰雨";
-        Mage.skill_atk = "冰霜雨";
+        Game_role_setting Archer = new Game_role_setting("弓箭手",100,50,20,"腐蝕箭","集中","連續射擊");
+        Game_role_setting Berserker = new Game_role_setting("狂戰士",150,60,10,"戰斧","衝鋒","旋風斬");
+        Game_role_setting Mage = new Game_role_setting("魔法師",60,70,100,"法杖","火焰雨","冰霜雨");
 
         Archer.ShoutMyName();
 
