4d68e8f07b19a578ea2adb6758d2e2ced070418e
suyuu <93572182+suyunann@users.noreply.github.com>
2021-12-14 17:30:56 +0800

    Delete NKUST_C110181133 directory

--- NKUST_C110181133/IdeaProjects/NKUST_C110181133/NKUST/BMI.java
+++
@@ -1,8 +0,0 @@
-public class BMI {
-    public static void main (String[]args) {
-        float height = 1.75f;
-        int weight = 72;
-        float bmi = weight / (height * height);
-        System.out.println(bmi);
-    }
-}
