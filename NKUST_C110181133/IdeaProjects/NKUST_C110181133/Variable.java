0193e210f3f3e07154c77393f4c1d2e2cd47f750
suyuu <93572182+suyunann@users.noreply.github.com>
2021-12-14 12:59:58 +0800

    Add files via upload

---
+++ NKUST_C110181133/IdeaProjects/NKUST_C110181133/Variable.java
@@ -0,0 +1,10 @@
+public class Variable {
+    public static void main (String[]args){
+        float height = 1.75f;
+        int weight = 72;
+        float bmi = weight/(height*height);
+        System.out.println(bmi);
+
+
+    }
+}
